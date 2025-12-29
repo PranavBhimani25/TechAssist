cf86331ea95b365e57d37ddf1af7cb49b08993ee
PranavBhimani25 <pranavbhimani04@gmail.com>
2025-12-29 22:33:49 +0530

    Use PostgreSQL database

--- TechAssist/Program.cs
+++ TechAssist/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // --- JWT Authentication Setup ---
 var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
